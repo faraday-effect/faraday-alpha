b799f68f9498dffa85621397aa042675af563575
Tom Nurkkala <tnurkkala@cse.taylor.edu>
2019-05-07 11:07:43 -0400

    Add department e2e tests.

--- server/test/department.e2e-spec.ts
+++ server/test/department.e2e-spec.ts
@@ -1,45 +1,49 @@
-import { INestApplication, HttpServer } from "@nestjs/common";
+import { HttpServer, INestApplication } from "@nestjs/common";
 import { Test, TestingModule } from "@nestjs/testing";
-import request from "supertest";
-import { AppModule } from "../src/app.module";
-import { DepartmentService } from "../src/relational/department/department.service";
-import { Department } from "../src/relational/department/department.entity";
-import { Repository } from "typeorm";
 import { getRepositoryToken } from "@nestjs/typeorm";
+import request from "supertest";
+import { Repository } from "typeorm";
+import { AppModule } from "../src/app.module";
+import { Department } from "../src/relational/department/department.entity";
+import { DepartmentService } from "../src/relational/department/department.service";
 
 describe("Department (e2e)", () => {
+  let module: TestingModule;
   let app: INestApplication;
   let httpServer: HttpServer;
-  let deptService: DepartmentService;
-  let deptRepository: Repository<Department>;
+  let newDepartments: Department[];
 
   beforeAll(async () => {
-    const module: TestingModule = await Test.createTestingModule({
+    module = await Test.createTestingModule({
       imports: [AppModule]
     }).compile();
 
     app = module.createNestApplication();
     await app.init();
     httpServer = app.getHttpServer();
-
-    deptService = module.get(DepartmentService);
-    deptRepository = module.get(getRepositoryToken(Department));
   });
 
   afterAll(async () => {
-    app.close();
+    await app.close();
   });
 
-  beforeEach(() => {
-    deptRepository.query("TRUNCATE TABLE departments CASCADE");
+  beforeEach(async () => {
+    const deptNames = ["CS&E", "Physics", "English", "Music"];
+    const deptService: DepartmentService = module.get(DepartmentService);
+    const deptRepository: Repository<Department> = module.get(
+      getRepositoryToken(Department)
+    );
+
+    // Clear relevant database content.
+    await deptRepository.query("TRUNCATE TABLE departments CASCADE");
+
+    // Seed database.
+    newDepartments = await Promise.all(
+      deptNames.map(name => deptService.create({ name }))
+    );
   });
 
   it("can fetch all departments", async () => {
-    const deptNames = ["CS&E", "Physics", "English", "Music"];
-    for (let name of deptNames) {
-      await deptService.create({ name });
-    }
-
     return request(httpServer)
       .post("/graphql")
       .send({
@@ -53,6 +57,52 @@ describe("Department (e2e)", () => {
         `
       })
       .expect(200)
-      .then(response => expect(response.body.data.departments).toHaveLength(4));
+      .then(response => {
+        expect(response.body.data.departments).toHaveLength(4);
+      });
+  });
+
+  it("can fetch each department", async () => {
+    for (let newDept of newDepartments) {
+      await request(httpServer)
+        .post("/graphql")
+        .send({
+          query: /* GraphQL */ `
+            query oneDept($deptId: Int!) {
+              department(id: $deptId) {
+                id
+                name
+              }
+            }
+          `,
+          variables: { deptId: newDept.id }
+        })
+        .then(response => {
+          const dept = response.body.data.department;
+          expect(dept.id).toBe(newDept.id);
+          expect(dept.name).toBe(newDept.name);
+        })
+        .catch(error => console.error("ERROR", error));
+    }
+  });
+
+  it("can create a new department", async () => {
+    const newDeptName = "Underwater Handicrafts";
+    await request(httpServer)
+      .post("/graphql")
+      .send({
+        query: /* GraphQL */ `
+          mutation createOne($deptName: String!) {
+            createDepartment(data: { name: $deptName }) {
+              id
+              name
+            }
+          }
+        `,
+        variables: { deptName: newDeptName }
+      })
+      .then(response => {
+        expect(response.body.data.createDepartment.name).toBe(newDeptName);
+      });
   });
 });
