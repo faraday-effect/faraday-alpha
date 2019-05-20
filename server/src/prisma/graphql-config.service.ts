88b54fd7e48b0349c8ba74ada111d5920a5f3e3a
Tom Nurkkala <tnurkkala@cse.taylor.edu>
2019-05-19 23:27:13 -0400

    Add first custom Nexus field
    - Also configured typegenAutoConfig for better type inference

--- server/src/prisma/graphql-config.service.ts
+++ server/src/prisma/graphql-config.service.ts
@@ -1,10 +1,7 @@
 import { Injectable } from "@nestjs/common";
 import { GqlModuleOptions, GqlOptionsFactory } from "@nestjs/graphql";
-import {
-  makePrismaSchema,
-  prismaObjectType,
-  prismaExtendType
-} from "nexus-prisma";
+import { stringArg } from "nexus";
+import { makePrismaSchema, prismaObjectType } from "nexus-prisma";
 import path from "path";
 import datamodelInfo from "../generated/nexus-prisma";
 import { prisma } from "../generated/prisma-client";
@@ -19,7 +16,25 @@ export class GraphQLConfigService implements GqlOptionsFactory {
 
     const Mutation = prismaObjectType({
       name: "Mutation",
-      definition: t => t.prismaFields(["*"])
+      definition(t) {
+        t.prismaFields(["*"]);
+
+        t.field("createTrueFalseQuestion", {
+          type: "QuizQuestion",
+          args: {
+            title: stringArg({ required: true }),
+            text: stringArg({ required: true })
+          },
+          resolve: (_obj, { title, text }, ctx) => {
+            return ctx.prisma.createQuizQuestion({
+              type: "true-false",
+              title,
+              text,
+              details: { alpha: 17 }
+            });
+          }
+        });
+      }
     });
 
     const schema = makePrismaSchema({
@@ -33,13 +48,24 @@ export class GraphQLConfigService implements GqlOptionsFactory {
       outputs: {
         schema: path.join(__dirname, "../generated/schema.graphql"),
         typegen: path.join(__dirname, "../generated/nexus.ts")
+      },
+
+      typegenAutoConfig: {
+        sources: [
+          {
+            source: path.join(__dirname, "./types.ts"),
+            alias: "types"
+          }
+        ],
+        contextType: "types.Context"
       }
     });
 
     return {
       debug: true,
       playground: true,
-      schema
+      schema,
+      context: { prisma }
     };
   }
 }
