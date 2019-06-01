fab68dce9fa1f750a21387790dbc3cd0dac4f3cc
Tom Nurkkala <tnurkkala@cse.taylor.edu>
2019-06-01 11:25:33 -0400

    Rewrite quiz designer (WIP)

--- designer2/src/pages/QuizDesigner.vue
+++ designer2/src/pages/QuizDesigner.vue
@@ -1,18 +1,34 @@
 <template>
   <v-container grid-list-md>
+    <!-- Title and description -->
     <h6 class="title">Quiz Designer</h6>
+    <v-text-field label="Quiz Title" v-model="quizTitle"></v-text-field>
+    <v-textarea label="Description" rows="2" auto-grow> </v-textarea>
 
-    <!-- Title -->
-
-    <v-text-field
-      label="Quiz Title"
-      disabled="true"
-      v-model="quizTitle"
-    ></v-text-field>
-
-    <!-- Description -->
-
-    <v-textarea label="Description" rows="2" disable="true"> </v-textarea>
+    <!-- Questions -->
+    <h6 class="title">Questions</h6>
+    <v-layout row wrap>
+      <v-flex xs12 md3>
+        <v-list>
+          <v-list-tile v-for="i in 6" :key="i" :to="`/${i}`">
+            <v-list-tile-action>
+              <v-icon>dashboard</v-icon>
+            </v-list-tile-action>
+            <v-list-tile-content>
+              <v-list-tile-title>{{ `Zippy ${i}` }}</v-list-tile-title>
+            </v-list-tile-content>
+          </v-list-tile>
+        </v-list>
+      </v-flex>
+      <v-flex xs12 md9>
+        <p>
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Id tempora
+          repellat vitae officia dolore laudantium ipsam modi, reiciendis nulla
+          nihil eveniet quae molestias necessitatibus, aut accusantium!
+          Accusamus blanditiis quidem corrupti!
+        </p>
+      </v-flex>
+    </v-layout>
   </v-container>
 </template>
 
