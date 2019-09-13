907f391003bb375130d4a42e334812d39e2c496f
Tom Nurkkala <tnurkkala@cse.taylor.edu>
2019-09-13 12:56:11 -0400

    Fix Quick Table goofiness
    - Not trying to update the backing data on `@insert`
    - Using `@blur` instead.
      Seems to work fine.
      User experience just as good.

--- designer3/src/components/multi-morph/QuickTable.vue
+++ designer3/src/components/multi-morph/QuickTable.vue
@@ -3,22 +3,20 @@
     <table>
       <tbody>
         <tr v-for="(row, rowIdx) in data" :key="rowIdx">
-          <td v-for="(col, colIdx) in row" :key="colIdx">
+          <td class="datum" v-for="(col, colIdx) in row" :key="colIdx">
             <div
               contenteditable="true"
+              v-text="col"
               style="display: inline-block"
               :id="`${rowIdx}-${colIdx}`"
-              @input="changeMe"
-            >
-              {{ col }}
-            </div>
+              @blur="updateCell"
+            />
           </td>
         </tr>
       </tbody>
     </table>
     <button @click="addRow">Add Row</button>
     <button @click="addCol">Add Col</button>
-    <button @click="dumpData">Dump Data</button>
     <div>{{ data }}</div>
   </div>
 </template>
@@ -36,15 +34,14 @@ export default Vue.extend({
     addCol() {
       this.data.forEach(row => row.push("salad"));
     },
-    dumpData() {
-      console.log(this.data);
-    },
-    changeMe(event) {
-      const td = event.target;
-      const [row, col] = td.id.split("-");
-      const newValue = td.innerText;
-      console.log(row, col, newValue);
-      console.log(this.data);
+    updateCell(event) {
+      const lostFocus = event.target;
+      const gotFocus = event.relatedTarget;
+      console.log("BLUR", lostFocus, gotFocus);
+
+      const [row, col] = lostFocus.id.split("-");
+      const newValue = lostFocus.innerText;
+      console.log(`(${row}, ${col}) => ${newValue}`);
       this.$set(this.data[row], col, newValue);
     }
   },
@@ -59,3 +56,10 @@ export default Vue.extend({
   }
 });
 </script>
+
+<style>
+.datum {
+  background: lightgray;
+  border: thin dashed coral;
+}
+</style>
--- designer3/stories/index.js
+++ designer3/stories/index.js
@@ -1,14 +1,13 @@
 import { storiesOf } from "@storybook/vue";
-import MyButton from "./Button.vue";
 import QuickTable from "../src/components/multi-morph/QuickTable";
 
-storiesOf("Button", module)
-  .add("with text", () => "<my-button>with text</my-button>")
-  .add("with emoji", () => "<my-button>😀 😎 👍 💯</my-button>")
-  .add("as a component", () => ({
-    components: { MyButton },
-    template: '<my-button :rounded="true">rounded</my-button>'
-  }));
+// storiesOf("Button", module)
+//   .add("with text", () => "<my-button>with text</my-button>")
+//   .add("with emoji", () => "<my-button>😀 😎 👍 💯</my-button>")
+//   .add("as a component", () => ({
+//     components: { MyButton },
+//     template: '<my-button :rounded="true">rounded</my-button>'
+//   }));
 
 storiesOf("QuickTable", module).add("basic", () => ({
   components: { QuickTable },
